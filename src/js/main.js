b6165dd161220a2d7f0a1f7b0bb436373c2fda58
teichopsia <anthomelanous@hotmail.com>
2017-09-28 17:08:02 -0500

    Comment out JS code for old navigation bar

--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,25 @@
+document.addEventListener("DOMContentLoaded", function(event){
+    console.log("DOM fully loaded and parsed");
+});
+
+window.addEventListener("load", function(event){
+    console.log("All resources finished loading");
+});
+
+/* Code not being used.
 // Navbar overlay
 document.addEventListener("DOMContentLoaded", function(event){
     // Open menu
-    var open = document.querySelector(".openbtn");
+    //var open = document.querySelector(".openbtn");
     open.addEventListener("click", function openNav(event) {
         var overlay = document.querySelector(".overlay");
         overlay.style.height = "100%";
     });
     // Close menu
-    var close = document.querySelector(".closebtn");
+    //var close = document.querySelector(".closebtn");
     close.addEventListener("click", function closeNav(event) {
         var overlay = document.querySelector(".overlay");
         overlay.style.height = "0%";
     })
 });
-
+*/
