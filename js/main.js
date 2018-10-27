e3c91a28bc93a1ba5e7232edfa6b6805f145dd78
Alexader <anthomelanous@hotmal.com>
2018-10-27 17:54:34 -0500

    Website re-design

---
+++ js/main.js
@@ -0,0 +1,26 @@
+function createMap(elemId, centerLat, centerLng, zoom) {
+    var map = new L.Map(elemId);
+ 
+    // Data provider
+    var osmUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+    var osmAttrib = 'Map data © <a href="https://openstreetmap.org">OpenStreetMap</a> contributors';
+ 
+    // Layer
+    var osmLayer = new L.TileLayer(osmUrl, {
+        minZoom: 4,
+        maxZoom: 20,
+        attribution: osmAttrib
+    });
+ 
+    // Map
+    map.setView(new L.LatLng(centerLat, centerLng), zoom);
+    map.addLayer(osmLayer);
+    return map;
+}
+	
+document.addEventListener("DOMContentLoaded", function () {
+    // The DOMContentLoaded event fires when the page is ready (like $(document).ready() in jQuery)
+    var map = createMap('mapid', 8.9529, -79.4353, 6);
+    // map is the id of our div element and the other options are
+    // lattitude, longitude and zoom
+});
